0e54ff7d6d55d9ba46bf5d7e7ed134788956e3cd
christiancabarrocas <christiancabarrocas@gmail.com>
2020-10-28 12:36:19 +0100

    Update

--- Sources/Console/Console.swift
+++ Sources/Console/Console.swift
@@ -16,18 +16,23 @@ enum ConsoleErrorType: String {
     case unknown
 }
 
+enum ConsoleLogType {
+    case info
+    case debug
+    case error
+}
+
 @available(iOS 14.0, *)
 struct Console {
 
     private static var subsystem = Bundle.main.bundleIdentifier!
 
-    static func log(_ message: String = "", type: OSLogType = .info, subtype: ConsoleErrorType = .none) {
+    static func log(_ message: String = "", type: ConsoleLogType = .info, subtype: ConsoleErrorType = .none) {
         let customLog = Logger(subsystem: subsystem, category: subtype.rawValue)
         switch type {
         case .debug: customLog.debug("\(message)")
         case .error: customLog.error("\(message)")
         case .info: customLog.info("\(message)")
-        default: customLog.log("\(message)")
         }
     }
 }
