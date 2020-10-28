488de3eea531175d81d664be057b458365f14690
christiancabarrocas <christiancabarrocas@gmail.com>
2020-10-28 12:26:36 +0100

    First commit

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Console
 
-A description of this package.
+A small wrapper around os.log 
--- Sources/Console/Console.swift
+++ Sources/Console/Console.swift
@@ -1,3 +1,33 @@
+//
+//  Console.swift
+//  Created by Christian Cabarrocas on 14/06/2019.
+//  Basic os.log wrapper
+
+import Foundation
+import os.log
+
+enum ConsoleErrorType: String {
+    case networking
+    case decoding
+    case encoding
+    case viewCycle
+    case userInterface
+    case none
+    case unknown
+}
+
+@available(iOS 14.0, *)
 struct Console {
-    var text = "Hello, World!"
+
+    private static var subsystem = Bundle.main.bundleIdentifier!
+
+    static func log(_ message: String = "", type: OSLogType = .info, subtype: ConsoleErrorType = .none) {
+        let customLog = Logger(subsystem: subsystem, category: subtype.rawValue)
+        switch type {
+        case .debug: customLog.debug("\(message)")
+        case .error: customLog.error("\(message)")
+        case .info: customLog.info("\(message)")
+        default: customLog.log("\(message)")
+        }
+    }
 }
